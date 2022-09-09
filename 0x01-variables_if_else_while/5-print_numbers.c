4a4176df643fd02dbe5d063866c744f4082fc9e1
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-09 17:56:35 +0100

    666

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Main function entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0 ; i < 10; i++)
+	{
+		printf("%d", i);
+	}
+	/* print new line */
+	putchar('\n');
+	return (0);
+}
