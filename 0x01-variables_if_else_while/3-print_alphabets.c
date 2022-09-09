756969665391d4252d0c16f779247856c07485a3
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-09 15:18:27 +0100

    putchar

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Main function entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	char alphabets[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+	int len = strlen(alphabets);
+	int i;
+
+	for (i = 0 ; i < len; i++)
+	{
+		putchar(alphabets[i]);
+	}
+	/* print new line */
+	putchar('\n');
+	return (0);
+}
