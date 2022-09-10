1c8955760091eead8fbc3dd870917eb12d66d97f
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-10 12:27:37 +0100

    success

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Main function entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* declare variables */
+	int int_a;
+	int int_b;
+	int int_c;
+	/* int i; */
+
+	/* loop for the first integer */
+	for (int_a = 0 ; int_a < 8; int_a++)
+	{
+		/* add one to int a */
+		int_b = int_a + 1;
+		/* loop for second iinteger */
+		for (; int_b < 9; int_b++)
+		{
+			/* add 1 to int_b */
+			int_c = int_b + 1;
+			for (; int_c < 10; int_c++)
+			{
+				putchar(int_a + '0');
+				putchar(int_b + '0');
+				putchar(int_c + '0');
+				/* skip last comma */
+				if (int_a != 7 || int_b != 8 || int_c != 9)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	/* end new line */
+	putchar('\n');
+	return (0);
+}
