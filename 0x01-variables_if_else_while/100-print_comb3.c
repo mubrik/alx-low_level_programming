981a1d972c120e58a70b1231f55e7111314c7bbf
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-10 12:01:35 +0100

    100ez

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * main - Main function entry point
+ * Return: Always 0
+ */
+int main(void)
+{
+	/* declare variables */
+	int int_a;
+	int int_b;
+	/* int i; */
+
+	/* loop for the first integer */
+	for (int_a = 0 ; int_a < 9; int_a++)
+	{
+		/* putchar(int_a + '0'); */
+		/* add one to previous int */
+		int_b = int_a + 1;
+		/* loop for second iinteger */
+		for (; int_b < 10; int_b++)
+		{
+			putchar(int_a + '0');
+			putchar(int_b + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	return (0);
+}
