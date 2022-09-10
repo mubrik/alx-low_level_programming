bfa8c46495d76972ef3d57dfc59e74219ef03f9a
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-10 20:48:59 +0100

    commenting

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -8,27 +8,34 @@
 int main(void)
 {
 	/* declare variables */
-	int int_aa;
-	int int_ab;
-	int int_ba;
-	int int_bb;
-
-	for (int_aa = 0; int_aa < 10; int_aa++)
+	int int_first_a;
+	int int_first_b;
+	int int_second_a;
+	int int_second_b;
+	/* first a loop */
+	for (int_first_a = 0; int_first_a < 10; int_first_a++)
+	/* first b loop */
 	{
-		for (int_ab = 0; int_ab < 10; int_ab++)
+		for (int_first_b = 0; int_first_b < 10; int_first_b++)
 		{
-			for (int_ba = int_aa; int_ba < 10; int_ba++)
+			/* second a loop, notice we are tracking first a here!, why? because after first a passes a number, */
+			/* we can be sure all possible match with that number is done, e.g if first a passes 40 like: 4* **, */
+			/* ** 2* is unnecassary because it must have been matched earlier */
+			for (int_second_a = int_first_a; int_second_a < 10; int_second_a++)
 			{
-				for (int_bb = int_ba == int_aa ? int_ab + 1 : 0;
-					 int_bb < 10; int_bb++)
+				/* we determine the start value by comparing if first a matches second a, but up above we set second a to be equal first a, so why match?? */
+				/* because that we we can track if it's on its first iteration, first iteration means we can add one to first b as the satrt value,*/
+				/* because again it must have been matched earlier */
+				for (int_second_b = int_second_a == int_first_a ? int_first_b + 1 : 0;
+					 int_second_b < 10; int_second_b++)
 				{
-					putchar(int_aa + '0');
-					putchar(int_ab + '0');
+					putchar(int_first_a + '0');
+					putchar(int_first_b + '0');
 					putchar(' ');
-					putchar(int_ba + '0');
-					putchar(int_bb + '0');
+					putchar(int_second_a + '0');
+					putchar(int_second_b + '0');
 					/* skip last number */
-					if (int_aa != 9 || int_ab != 8)
+					if (int_first_a != 9 || int_first_b != 8)
 					{
 						putchar(',');
 						putchar(' ');
