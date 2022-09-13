6fe7093a2da1d4019ab0c3553bb6e1994ac89e0c
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-13 12:54:11 +0100

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,15 @@
+/**
+ * _isalpha - checks if char is alphabetic
+ * @c: the character to check
+ * Return: 1 if c is alphabetic lower case or upper
+ * returns 0 otherwise
+ */
+int _isalpha(int c)
+{
+	/* simple if check */
+	if ( (c > 96 && c < 123) || (c > 64 && c < 91))
+	{
+		return (1);
+	}
+	return (0);
+}
