59f2527f901229925ef62f08d2139e5366207f9b
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-13 21:03:31 +0100

    6taskfix

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -9,6 +9,8 @@ int print_last_digit(int n)
 {
 	int last_digit;
 	/* get diff divided 10 */
+	if (n < 0)
+		n = n * -1;
 	last_digit = abs(n) % 10;
 	_putchar(last_digit + '0');
 	return (last_digit);
