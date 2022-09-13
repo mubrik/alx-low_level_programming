6e8cf9fa3d8931a9f97812b93e9d1fd33d444aab
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-13 12:46:21 +0100

    islower

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,14 @@
+/**
+ * _islower - prints out alphabet in low case 10 times
+ * @c: the character to check
+ * Return: 1 if c is lower case, returns 0 otherwise
+ */
+int _islower(int c)
+{
+	/* simple if check */
+	if ( c > 96 && c < 123 )
+	{
+		return (1);
+	}
+	return (0);
+}
