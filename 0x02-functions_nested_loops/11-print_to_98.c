14507b2b49c0ca567c71989d5a96382843feb8dd
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-13 19:24:27 +0100

    11done

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -1,13 +1,73 @@
+#include <math.h>
 #include "main.h"
 
 /**
- * add - add 2 integers
- * @a: int a
- * @b: int b
- * Return: int
+ * print_3_num - print up to 3 numbers using _putchar
+ * @n: num to print
+ * Return: void
  */
-int add(int a, int b)
+void print_3_num(int n)
 {
-	/* what did yall expect? */
-	return (a + b);
+	if (n < 0)
+	{
+		/* print dash first, then remove negative */
+		_putchar('-');
+		if (n > -10)
+			_putchar((n * -1) + '0');
+		else
+		{
+			n = n * -1;
+			_putchar((n / 10) + '0');
+			_putchar((n % 10) + '0');
+		}
+	}
+	else if (n <= 9)
+		_putchar(n + '0');
+	else if (n >= 100)
+	{
+		/* print first, second and last char */
+		_putchar((n / 100) + '0');
+		_putchar(((n % 100) / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+	else
+	{
+		/* get first and last char */
+		_putchar((n / 10) + '0');
+		_putchar((n % 10) + '0');
+	}
+}
+
+/**
+ * print_to_98 - print from n to 98
+ * @n: int to start
+ * Return: void
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	if (n <= 98)
+	{
+		for (i = n; i <= 98; i++)
+		{
+			print_3_num(i);
+			if (i == 98)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	else
+	{
+		for (i = n; i >= 98; i--)
+		{
+			print_3_num(i);
+			if (i == 98)
+				continue;
+			_putchar(',');
+			_putchar(' ');
+		}
+	}
+	_putchar('\n');
 }
