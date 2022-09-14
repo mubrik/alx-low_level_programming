f82696ad102aab217975707b487df57cf9ed23a5
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-14 18:48:55 +0100

    complete

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,23 +8,23 @@
 int main(void)
 {
 	int i;
-	int first_numb;
-	int second_numb;
-	int sequence_val;
+	unsigned long int first_numb;
+	unsigned long int second_numb;
+	unsigned long int sequence_val;
 
 	/* first 2 numbers to start */
 	first_numb = 1;
 	second_numb = 2;
 
 	/* print first two terms */
-	printf("%d, %d, ", first_numb, second_numb);
+	printf("%lu, %lu, ", first_numb, second_numb);
 
 	/* start by adding both */
 	sequence_val = first_numb + second_numb;
 
 	for (i = 3; i <= 50; i++)
 	{
-		printf("%d, ", abs(sequence_val));
+		printf("%lu ", sequence_val);
 		/* make first numb 2nd numb */
 		first_numb = second_numb;
 		/* make second numb the prev total */
