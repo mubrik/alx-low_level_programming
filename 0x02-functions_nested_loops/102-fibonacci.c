eda08c52ff2228f85e95d0f4285cd967405dab3d
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-14 18:43:58 +0100

    fic2

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -2,30 +2,6 @@
 #include <stdlib.h>
 
 /**
- * fib - fibonacci recursive
- * @n: fibonnaci seq
- * Return: void
- */
-int fib(int n)
-{
-	int result;
-
-	if (n == 0)
-	{
-		return (0);
-	}
-	else if (n == 1)
-	{
-		return 1;
-	}
-	else
-	{
-		result = fib(n - 1) + fib(n - 2);
-		return(result);
-	}
-}
-
-/**
  * main - prints fib seq to 50
  * Return: 0
  */
