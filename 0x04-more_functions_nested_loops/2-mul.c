bb838df94f718620c3378cc65e9065146bd0eb58
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-15 13:33:25 +0100

    Commited from Script

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+  * mul - multiply a nad b
+	* @a: integer to multiply
+	* @b: integer to multiply
+	* Return: result of multiply
+	*/
+int mul(int a, int b)
+{
+	return (a * b);
+}
