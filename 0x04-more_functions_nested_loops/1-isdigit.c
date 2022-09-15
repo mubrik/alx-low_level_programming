a3c8a31be360788120d4e3925d9de6ba13ce506c
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-15 13:27:53 +0100

    Commited from Script

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+  * _isdigit - checks if int in range 0-9
+	* @c: integer to check
+	* Return: 1 if digit, 0 if not
+	*/
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+		return (1);
+	return (0);
+}
