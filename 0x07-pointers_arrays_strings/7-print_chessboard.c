b55d5ca3d64f807a371d19b6569384fb9aa0f163
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-27 16:00:07 +0100

    fix3

--- 0x07-pointers_arrays_strings/7-print_chessboard.c
+++ 0x07-pointers_arrays_strings/7-print_chessboard.c
@@ -1,27 +1,32 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
   * print_chessboard - prints the chessboard.
-	* @str_ptr: pointer of char
+	* @ptr_to_ptr: pointer to ptr
 	* Return: void
 	*/
-void print_chessboard(char (*str_ptr)[8])
+void print_chessboard(char (*ptr_to_ptr)[8])
 {
+
 	/* variables */
 	int col;
 	char *row_ptr;
-	/* copy pointer */
-	row_ptr = *str_ptr;
+	/* copy first value which is a pointer to the 1st row ptr/add */
+	row_ptr = *ptr_to_ptr;
 
-	/* iterate using row ptr then push by a full row size */
-	for (; *row_ptr != '\0'; row_ptr = row_ptr + 8)
+	/* iterate using row ptr then push by a full row size to
+		* move pointer to start of current row
+		*/
+	while (*row_ptr != '\0')
 	{
 		/* get col by pushing with a size of a single value */
 		for (col = 0; col < 8; col++)
 		{
-			_putchar(*(row_ptr + col));
+			_putchar(row_ptr[col]);
 		}
 		_putchar('\n');
+		row_ptr = row_ptr + (sizeof(char) * 8);
 	}
 
 }
