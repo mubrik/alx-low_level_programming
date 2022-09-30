48a764d41178f36f5ab031ea481ef5416ead584e
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-09-30 14:22:26 +0100

    bettypls

--- 0x0A-argc_argv/2-args.c
+++ 0x0A-argc_argv/2-args.c
@@ -11,7 +11,7 @@ int main(int argc, char *argv[])
 {
 	int index;
 
-	for(index = 0; index <= argc - 1; index++)
+	for (index = 0; index <= argc - 1; index++)
 		printf("%s\n", argv[index]);
 
 	return (0);
