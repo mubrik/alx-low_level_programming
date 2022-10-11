b140243fe213e7e0e17dd2042b6e2a9cef9a88d6
Mubarak Yahaya <mubarakg4u@gmail.com>
2022-10-11 15:05:29 +0100

    test

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,15 @@
+#ifndef DOGGO_H
+#define DOGGO_H
+/**
+ * struct dog - Describes a dog
+ * @name: char ptr name of dog
+ * @owner: char ptr owner of dog
+ * @age: float age of dog
+ */
+struct dog
+{
+	char *name;
+	char *owner;
+	float age;
+};
+#endif /* DOGGO_H */
