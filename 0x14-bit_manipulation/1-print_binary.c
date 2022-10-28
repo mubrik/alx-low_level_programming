8ca08771a45b12a5b023cb9869ce59f212bece8f
mubrik <mubarakg4u@gmail.com>
2022-10-28 22:49:34 +0100

    Commited from Script

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -8,7 +8,8 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int bitmask, print_flag = 0, bit_count = 0;
+	unsigned int print_flag = 0, bit_count = 0;
+	unsigned long int bitmask;
 
 	if (n == 0)
 	{
@@ -16,11 +17,11 @@ void print_binary(unsigned long int n)
 		return;
 	}
 	/* 100000...  */
-	bitmask = 1 << 31;
+	bitmask = 1UL << 63;
 	/* loop through bits and perform & to keep only 1/true bit */
-	for (; bit_count < 32; bit_count++)
+	for (; bit_count < 64; bit_count++)
 	{
-		print_flag = print_flag ? print_flag : (n & bitmask);
+		print_flag = print_flag ? print_flag : ((n & bitmask) > 0);
 		if (print_flag)
 			_putchar(n & bitmask ? '1' : '0');
 		n <<= 1; /* shift value by 1 to keep push */
