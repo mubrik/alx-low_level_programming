95d9baa402c8a0cecc2c8ee4d6dbddf8c2412ec6
mubrik <mubarakg4u@gmail.com>
2022-10-31 16:57:02 +0100

    Commited from Script

--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -2,6 +2,24 @@
 #include "main.h"
 
 /**
+  * _strlen - returns the length of a string.
+	* @src_ptr: pointer of char
+	* Return: int length
+	*/
+int _strlen(char *src_ptr)
+{
+	/* checking if the current value of pointer not null */
+	if (*src_ptr != 0)
+	{
+		/* move the pointer foward and call the function again adding 1! */
+		src_ptr++;
+		return (1 + _strlen(src_ptr));
+	}
+
+	return (0);
+}
+
+/**
  * append_text_to_file - appends text at the end of a file.
  * @filename: file name/path
  * @text_content: text buffer to add to file
