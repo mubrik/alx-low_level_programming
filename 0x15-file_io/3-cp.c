ff1efa794476dce01e10981f8690bcd783cf20e5
mubrik <mubarakg4u@gmail.com>
2022-10-31 17:06:14 +0100

    Commited from Script

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -20,29 +20,24 @@ int _strlen(char *src_ptr)
 }
 
 /**
- * create_file -  creates a file..
+ * append_text_to_file - appends text at the end of a file.
  * @filename: file name/path
  * @text_content: text buffer to add to file
  * Return: unsigned int.
  */
-int create_file(const char *filename, char *text_content)
+int append_text_to_file(const char *filename, char *text_content)
 {
 	unsigned int fd;
-	int w_val = 0;
 
 	if (!filename)
 		return (-1);
 	/* attempt to open in rdonly */
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	fd = open(filename, O_RDWR | O_APPEND);
 	if (!fd)
 		return (-1);
 
 	if (text_content)
-	{
-		w_val = write(fd, text_content, _strlen(text_content));
-		if (w_val < 0)
-			return (-1);
-	}
+		write(fd, text_content, _strlen(text_content));
 
 	close(fd);
 	return (1);
