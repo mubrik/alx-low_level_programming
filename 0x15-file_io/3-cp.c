cc3b8c8e7e19027a9aa3bd16e3cea39fc67279a0
mubrik <mubarakg4u@gmail.com>
2022-11-01 09:52:59 +0100

    Commited from Script

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -19,14 +19,14 @@ int main(int ac, char *av[])
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
+	/* read file from */
+	file_from_fd = open(av[1], O_RDONLY);
+	if (!file_from_fd)
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(98);
 	/* open file to / create */
 	file_to_fd = open(av[2], O_RDWR | O_CREAT | O_TRUNC, 0664);
 	if (!file_to_fd)
 		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", av[2]), exit(99);
-	/* read file from */
-	file_from_fd = open(av[1], O_RDONLY);
-	if (!file_from_fd)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", av[1]), exit(99);
 
 	pr_buff = malloc(BUFF_SIZE);
 	if (!pr_buff)
