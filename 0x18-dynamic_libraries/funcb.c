f7d6741869a9adf7f284c9d7006a4192b72cc682
mubrik <mubarakg4u@gmail.com>
2022-12-12 22:16:59 +0100

    Commited from Script

---
+++ 0x18-dynamic_libraries/funcb.c
@@ -0,0 +1,56 @@
+#include "main.h"
+
+/**
+ * add - add 2 ints
+ * @a: int a
+ * @b: int b
+ * Return: result f add
+ */
+int add(int a, int b)
+{
+	return (a + b);
+}
+
+/**
+ * sub - sub 2 ints
+ * @a: int a
+ * @b: int b
+ * Return: result of sub
+ */
+int sub(int a, int b)
+{
+	return (a - b);
+}
+
+/**
+ * mul - mul 2 ints
+ * @a: int a
+ * @b: int b
+ * Return: result of mul
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
+
+/**
+ * div - div 2 ints
+ * @a: int a
+ * @b: int b
+ * Return: result of div
+ */
+int div(int a, int b)
+{
+	return (a / b);
+}
+
+/**
+ * mod - mod 2 ints
+ * @a: int a
+ * @b: int b
+ * Return: result of mod
+ */
+int mod(int a, int b)
+{
+	return (a % b);
+}
