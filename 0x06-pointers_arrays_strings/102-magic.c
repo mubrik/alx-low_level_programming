bcde8da2d9d0ae6c07d9d3f6b46d7a453a9f949b
mubrik <mubarakg4u@gmail.com>
2023-01-22 10:29:38 +0100

    Commited from Script

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -19,5 +19,12 @@ int main(void)
   *(p + (sizeof(int) + 1)) = 98;
   /* ...so that this prints 98\n */
   printf("a[2] = %d\n", a[2]);
+  printf("size of an int = %ld\n", sizeof(int));
+  printf("memory of n %p\n", &n);
+  printf("memory of a %p\n", &a);
+  printf("memory of a[0] %p\n", &a[0]);
+  printf("memory of a[1] %p\n", &a[1]);
+  printf("memory of a[2] %p\n", &a[2]);
+  printf("memory of p + 5 %p\n", &n + 5);
   return (0);
 }
